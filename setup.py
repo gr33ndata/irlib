7046a774703be6188b893b121aba4891a8cb6114
Tarek Amr <gr33ndata@yahoo.com>
2012-12-25 14:01:47 +0000

    housekeeping

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name='IRLib',
+	name='irlib',
 	version='0.1.0',
 	author='Tarek Amr',
 	author_email='',
+    url='https://github.com/gr33ndata/irlib',
 	packages=['irlib'],
-	scripts=['bin/classify.py'],
-	url='http://pypi.python.org/pypi/IRLib/',
 	license='LICENSE.txt',
 	description='Inforamtion Retrieval Library',
-	long_description=open('README.md').read(),
+	long_description=open('README.rst').read(),
+    install_requires=["setuptools"],
 )
