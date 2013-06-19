6065153098987b638b159d0b2ef8e872de3e1cce
Tarek Amr <gr33ndata@yahoo.com>
2013-06-19 09:33:23 +0100

    refactoring LM and adding stats to it

--- irlib/lm.py
+++ irlib/lm.py
@@ -12,10 +12,35 @@ from superlist import SuperList
 from progress import Progress
 from preprocessor import Preprocessor
 
+class UnseenTerms:
+
+    def __init__(self):
+        self.counts = {}
+        
+    def update(self, doc_id='', unseen=True):
+        if not doc_id in self.counts:
+            self.counts[doc_id] = {
+                'seen': 0,
+                'unseen': 0
+            }       
+        if unseen:
+            self.counts[doc_id]['unseen'] += 1
+        else:
+            self.counts[doc_id]['seen'] += 1
+
+    def display(self):
+        for doc_id in self.counts:
+            unseen = self.counts[doc_id]['unseen'] 
+            total = self.counts[doc_id]['unseen'] + self.counts[doc_id]['seen']
+            unseen_ratio =  unseen * 1.0 / total
+            print 'Documdent:', doc_id
+            print '\tUnseen ratio:', unseen_ratio 
+    
+    
 class LM:
 
     
-    def __init__(self, n=3, lpad='', rpad='', joiner='', 
+    def __init__(self, n=3, lpad='', rpad='', laplace_gama=1, 
                  verbose=False):
         '''
         Initialize our LM
@@ -34,13 +59,19 @@ class LM:
         self.term_count_n_1 = {}
         # The vocabulary of all classes (for Laplace smoothing)
         self.vocabulary = set()
-        # All possible n-1 words, used for Laplace smoothing
-        self.n_1_vocabulary = 0
         self.lpad=lpad
         self.rpad=rpad
-        self.joiner = joiner
+        self.laplace_gama = laplace_gama
+        self.joiner = ' '
+        self.unseen_counts = UnseenTerms()
         self.verbose = verbose
 
+    def display(self):
+        '''
+        Displays statistics about our LM
+        '''
+        self.self.unseen_counts.display()
+    
     def to_ngrams(self, terms):
         ''' Converts terms to all possibe ngrams 
             terms: list of terms
@@ -93,19 +124,37 @@ class LM:
         ngrams = self.to_ngrams(terms)    
         self.update_counts(doc_id, ngrams)  
     
+    def laplace(self, x, y, doc_id):
+        add_numer = 1 * self.laplace_gama
+        laplace_mode = 'n-1'
+        if laplace_mode == 'ch':
+            v = len(self.vocabulary)
+        elif laplace_mode == 'ch^n-1':
+            v = math.pow(len(self.vocabulary),self.n-1)
+        else:
+            v = len(self.term_count_n_1[doc_id]['ngrams'])
+        add_denom = v * self.laplace_gama
+        return float(x + add_numer) / float(y + add_denom)
+            
+        
     def pr_ngram(self, doc_id, ngram, smooting='Laplace', log=True, logbase=2):
-        add_numer = 0
-        add_denom = 0
-        if smooting == 'Laplace':
-            add_numer = 1
-            #add_denom = len(self.vocabulary)
-            if not self.n_1_vocabulary:
-                self.n_1_vocabulary = math.pow(len(self.vocabulary),self.n-1)
-            add_denom = self.n_1_vocabulary
         ngram_n = self.joiner.join(ngram)
         ngram_n_1 = self.joiner.join(ngram[:-1])
-        pr = float(self.term_count_n[doc_id]['ngrams'].get(ngram_n,0) + add_numer) \
-             / float(self.term_count_n_1[doc_id]['ngrams'].get(ngram_n_1,0) + add_denom)
+        ngram_n_count = self.term_count_n[doc_id]['ngrams'].get(ngram_n,0)
+        ngram_n_1_count = self.term_count_n_1[doc_id]['ngrams'].get(ngram_n_1,0)
+        # Apply smoothing
+        if smooting == 'Laplace':
+            pr = self.laplace(ngram_n_count, ngram_n_1_count, doc_id)
+        elif smooting == 'Witten':
+            # We need to implement this
+            pass
+        else:
+            pr = float(ngram_n_count) / float(ngram_n_1_count)
+        # Update seen/unseen counts
+        if ngram_n_count:    
+            self.unseen_counts.update(doc_id=doc_id, unseen=False)
+        else:
+            self.unseen_counts.update(doc_id=doc_id, unseen=True)
         if self.verbose:
             print ngram, pr
         if log:
@@ -158,112 +207,20 @@ class LM:
         return calculated     
             
                 
-class LM_Old:
 
-    ''' This is to be deleted, replaced with LM now'''
-
-    def __init__(self, n=3, verbose=False):
-        '''
-        Initializes our Language Model
-        ngram = n-gram (default = 3)
-        term_count = {
-            'orange': {
-                'total': 3, 
-                'terms': {'juice': 1, 'naranja': 1, 'jaffa': 1}
-            }, 
-            'apple': {
-                'total': 6, 
-                'terms': {'ipad': 2, 'ios': 3, 'apple': 1}
-            }
-        }   
-        '''
-        self.verbose = verbose
-        self.n = n
-        self.term_count = {}
-        self.vocabulary = set()
-    
-    def get_vocabulary(self):
-        return list(self.vocabulary)
-        
-    def logpr(self, x, base=2):
-        if x == 0:
-            return -math.log(0.0000001,base)
-        else:
-            return -math.log(x,base)
-    
-    def term_pr(self, doc_id, term, smooting='Laplace', log=True, logbase=2):
-        add_numer = 0
-        add_denom = 0
-        if smooting == 'Laplace':
-            add_numer = 1
-            add_denom = len(self.vocabulary)
-        pr = float(self.term_count[doc_id]['terms'].get(term,0) + add_numer) \
-             / float(self.term_count[doc_id]['total'] + add_denom)
-        if self.verbose:
-            print term, pr
-        if log:
-            return -math.log(pr,logbase)
-        else:
-            return pr
-                
-    def ngrams(self, terms):
-        ''' Converts terms to all possibe ngrams 
-            terms: list of terms
-        '''
-        if self.n == 1:
-            return terms
-        n_grams = []
-        for i in range(0,len(terms)-self.n+1):
-            n_grams.append(" ".join(terms[i:i+self.n]))
-        return n_grams
-		    
-    def add_doc(self, doc_id = '', doc_terms=[]):
-        if not doc_id in self.term_count:
-            self.term_count[doc_id] = {'terms': {}, 'total': 0}
-        terms = self.ngrams(doc_terms)
-        for term in terms:
-            if term in self.term_count[doc_id]['terms']:
-                self.term_count[doc_id]['terms'][term] += 1
-            else:
-                self.term_count[doc_id]['terms'][term] = 1
-            self.term_count[doc_id]['total'] += 1
-            self.vocabulary.add(term)
-
-    def calculate(self, doc_terms=[], actual_id=''):
-        if self.verbose:
-            print self.term_count
-        calculated_id = {
-            'id_prob': -1,
-            'calc_id': '',
-            'actual_id': actual_id
-        }
-        terms = self.ngrams(doc_terms)
-        for doc_id in self.term_count:
-            #print '\n', doc_id, ':'
-            doc_pr = 0
-            for term in terms:
-                doc_pr += self.term_pr(doc_id, term, 
-                        smooting='Laplace', log=True, logbase=2)
-            if self.verbose:            
-                print doc_id, actual_id, doc_pr  
-            if calculated_id['id_prob'] == -1 or doc_pr < calculated_id['id_prob']:
-                calculated_id['id_prob'] = doc_pr
-                calculated_id['calc_id'] = doc_id
-        return calculated_id            
-    
 if __name__ == '__main__':
 
     p = Preprocessor()
     
-    lm = LM(n=3, verbose=True)
+    lm = LM(n=2, verbose=True)
     
     #print lm.to_ngrams(p.term2ch('hello dear world'))
     #sys.exit()
     
-    lm.add_doc(doc_id='apple', doc_terms=p.term2ch('i see glass of apple juice'))
+    #lm.add_doc(doc_id='apple', doc_terms=p.term2ch('i see glass of apple juice'))
     lm.add_doc(doc_id='apple', doc_terms=p.term2ch('the tree is full of apples'))
     #lm.add_doc(doc_id='orange', doc_terms=p.term2ch('i see the orange tree shaking'))
-    lm.add_doc(doc_id='orange', doc_terms=p.term2ch('orang juice'))
+    lm.add_doc(doc_id='orange', doc_terms=p.term2ch('orange orange juice'))
     #lm.add_doc(doc_id='orange', doc_terms=p.term2ch('i do not like jaffa cake'))
     #lm.add_doc(doc_id='apple', doc_terms=p.term2ch('i have apple juice'))
     #print lm.pr
