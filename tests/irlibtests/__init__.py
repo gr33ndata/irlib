a7eed563dcf26bb814841723b9cce1a456de024b
Tarek Amr <gr33ndata@yahoo.com>
2014-11-24 15:22:26 +0200

    Organizing tests into separate files

--- tests/irlibtests/__init__.py
+++ tests/irlibtests/__init__.py
@@ -1,20 +1,11 @@
 import os
 import sys
 
-def is_test_file(myfile, mypath):
-    if not os.path.isfile(os.path.join(mypath,myfile)):
-        return False
-    return (myfile.startswith('Test') and myfile.endswith('.py'))
-
 def get_irlibtests_path():
     return os.path.dirname(os.path.realpath(__file__))
 
 def get_irlib_path():
     return os.path.join(get_irlibtests_path(), '..', '..')
-
-def get_irlibtests_tests():
-    mypath = get_irlibtests_path()
-    return [f.rsplit('.',1)[0] for f in os.listdir(mypath) if is_test_file(f,mypath)]
 
 sys.path.append(get_irlib_path())
 
